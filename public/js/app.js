d71349268c182f4a8bb563a437b509087382e9dd
Chris Larsen <clarsen@rbcon.com>
2014-02-27 17:09:48 -0700

    working on delete button and getting action working properly

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,12 @@
 App = Ember.Application.create();
 
 App.Router.map( function() {
-    this.resource('uuids', { path: '/' }, function() {
-        this.resource('uuid', { path: '/:uuid' } );
-    });
+    this.resource('uuids', { path: '/' });
 });
 
 App.Uuid = DS.Model.extend({
     host_name: DS.attr(),
     host_uuid: DS.attr(),
-    uuid: DS.attr(),
     state: DS.attr()
 });
 
@@ -18,14 +15,14 @@ App.UuidAdapter = DS.RESTAdapter.extend({
     host: 'http://appdb01.qa0.mozyops.com:3000'
 });
 
-App.UuidsRoute = Ember.Route.extend({
+App.ApplicationRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('uuid');
-    }
-});
-
-App.UuidRoute = Ember.Route.extend({
-    model: function(params) {
-        return this.store.find('uuid', params.uuid);
+    },
+    actions: {
+        delete: function(id) {
+            var uuid = this.store.find('uuid', id);
+            uuid.destroyRecord();
+        }
     }
 });
