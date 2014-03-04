d67e20582ff6b2ae23051eb0a0eb1588c51b198b
Chris Larsen <clarsen801@gmail.com>
2014-03-03 23:53:43 -0700

    include meta: { count: count } in uuids index response

--- routes/uuids.js
+++ routes/uuids.js
@@ -13,7 +13,7 @@ exports.index = function(req, res) {
             res.jsonp(400, { status: 'ERROR', message: err });
         }
         else {
-            res.jsonp(200, { 'uuids': docs });
+            res.jsonp(200, { uuids: docs, meta: { count: docs.length } });
         }
     });
 };
