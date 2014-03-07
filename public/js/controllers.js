cca9d1fb69edcd1d183c4c24d2d464da75018256
Chris Larsen <clarsen801@gmail.com>
2014-03-07 16:16:48 -0700

    input fields at top of table used to search

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,5 +7,35 @@ var uuidControllers = angular.module('uuidMaster.controllers', []);
 uuidControllers.controller('uuidsCtrl', [ '$scope', 'Uuid', function($scope, Uuid) {
     var uuidReq = Uuid.findAll();
     $scope.uuids = uuidReq; 
-    $scope.headers = [ 'Hostname', 'UUID', 'Host UUID', 'Last Request', 'State' ];
+    $scope.headers = [
+        {
+            name :'Hostname',
+            value: 'host_name'
+        },
+        {
+            name: 'UUID',
+            value: 'id'
+        },
+        {
+            name: 'Host UUID',
+            value: 'host_uuid'
+        },
+        {
+            name: 'Last Request',
+            value: 'last_request'
+        },
+        {
+            name: 'State',
+            value: 'state'
+        },
+        {
+            name: '',
+            value: ''
+        }
+    ];
+    $scope.filterCriteria = {
+        pageNumber: 1,
+        sortDir: 1,
+        sortedBy: 'host_name'
+    }
 }]);
